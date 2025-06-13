ee713e0c76bd19774f2e0a7ffce3cc75d4f49272
Roman Fluekiger <roman.fluekiger@gmail.com>
2025-06-13 15:55:33 +0200

    initial commit

---
+++ src/main/java/ch/wiss/mongo165/Mongo165Application.java
@@ -0,0 +1,13 @@
+package ch.wiss.mongo165;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mongo165Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mongo165Application.class, args);
+	}
+
+}
---
+++ src/test/java/ch/wiss/mongo165/Mongo165ApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.wiss.mongo165;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mongo165ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
